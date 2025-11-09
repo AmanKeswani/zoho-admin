b01bf03e3be97bb832b9db514ba747c676d2e312
Aman keswani <keswaniaman137@gmail.com>
2025-11-09 07:22:57 +0530

    fix(ui): show server login errors, add show-password toggle, move service badge

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 import { UIProvider } from '../components/ui/provider'
+import ServiceBadge from '../components/ServiceBadge'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="dark">
       <UIProvider>
         <Component {...pageProps} />
+        <div className="fixed right-4 bottom-4 z-50">
+          <ServiceBadge />
+        </div>
       </UIProvider>
     </div>
   )
